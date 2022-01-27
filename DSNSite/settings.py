1900aa9ccbdc2f6208003cb1c1ff0d134ea008e0
unknown <giwayusuf12@gmail.com>
2022-01-27 09:05:41 +0100

    should work

--- DSNSite/settings.py
+++ DSNSite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!(4wn8jxsjc66=zkg934a9fmf*xmvf7@oj++uhp0=xqn1(&xsa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kelvin-mlproject.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    #'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
